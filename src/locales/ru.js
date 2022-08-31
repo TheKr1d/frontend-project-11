cb3b3782c2fab9fc948345b55aa7573252dda135
TheKr1d <epifanov.vv.spb@gmail.com>
2022-08-31 19:06:02 +0300

    add renderRSS

--- src/locales/ru.js
+++ src/locales/ru.js
@@ -2,6 +2,7 @@ export default {
   translation: {
     validRSS: 'RSS успешно загружен',
     errors: {
+      notValidRSS: 'Ресурс не содержит валидный RSS',
       notValidUrls: 'Ссылка должна быть валидным URL',
       workedRSS: 'RSS уже существует',
     },

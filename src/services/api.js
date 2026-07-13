01927cdde578915655151bfacabc4254938c4849
Vlad <epifanov.vv.spb@gmail.com>
2026-07-13 14:26:51 +0300

    fix hexlet tests

--- src/services/api.js
+++ src/services/api.js
@@ -7,7 +7,7 @@ export const fetchViaProxy = (url) => {
         .get(PROXY_URL, {
             params: {
                 url: url,
-                disableCache: true
+                disableCache: "true"
             },
             headers: {
                 'Accept': 'application/json'

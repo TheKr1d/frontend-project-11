5a0e0f9c69dfc8d13bd4096bd03c5d2a0a36628a
Vlad <epifanov.vv.spb@gmail.com>
2026-07-14 20:37:50 +0300

    logs errors fot hexlet tests

--- src/handles/handleFetch.js
+++ src/handles/handleFetch.js
@@ -8,19 +8,25 @@ export const handleFetch = (url) => {
   return fetchViaProxy(url)
     .then((response) => {
       let content;
+      console.log(1)
       try {
         content = rssParser(response.data.contents);
       } catch (parseError) {
         throw new Error('invalidRss', { cause: parseError });
       }
-      
+      console.log(2)
       const normaliseContent = getNormaliseContent(content, url);
-      
+      console.log(3)
       setFormState('uploaded', normaliseContent);
+      console.log(4)
       addContentState(normaliseContent);
+      console.log(5)
       const feedId = normaliseContent.feed.id;
+      console.log(6)
       const timerManager = createTimerManager();
+      console.log(7)
       timerManager.startTimer(feedId);
+      console.log(8)
       
       return normaliseContent;
     })
